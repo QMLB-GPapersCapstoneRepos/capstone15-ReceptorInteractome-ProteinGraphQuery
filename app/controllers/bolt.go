ced3911e3b6393e96913aaedf25cf0ebfbc5f9b4
Nick Janus <nickjanus@gmail.com>
2015-04-15 02:18:13 -0400

    Adds intial database population

--- app/controllers/bolt.go
+++ app/controllers/bolt.go
@@ -6,87 +6,95 @@ import (
 	"github.com/boltdb/bolt"
 	//"github.com/revel/revel"
 	"bytes"
-	"io"
+	"encoding/json"
 	"io/ioutil"
-	"json"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-var graphBucketName []byte
+const graphBucketName string = "graph"
 
 type GraphEntry struct {
-  Target byte[]
-  Score	byte[]
+	Target string
+	Score  float64
 }
 
-func (entry *GraphEntry) Encode() error {
-  result, err = json.Marshal(entry)
-  Check(err)
-  return result
+func (entry *GraphEntry) Encode() []byte {
+	result, err := json.Marshal(entry)
+	Check(err)
+	return result
 }
 
 func InitDB() {
 	dbName := "db/HumanPredictions.db"
-	graphBucketName := []byte("graph")
 
-	if _, err := os.Stat(dbName); os.IsNotExist(err) {
-		log.Println("Generating new database...")
-		db, err := bolt.Open(dbName, 0600, nil)
-		Check(err)
-		defer db.Close()
-		db.Update(func(tx *bolt.Tx) error {
-			_, err := tx.CreateBucket(graphBucketName)
-			Check(err)
-		})
+	//if _, err := os.Stat(dbName); os.IsNotExist(err) {
+	log.Println("Generating new database...")
+	db, err := bolt.Open(dbName, 0600, nil)
+	Check(err)
+	defer db.Close()
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(graphBucketName))
+		//Check(err)
+		log.Println(err)
+		return nil
+	})
 
-		entries, err := ioutil.ReadDir("public/predictions")
-		Check(err)
+	entries, err := ioutil.ReadDir("public/predictions")
+	Check(err)
 
-		for _, entry := range entries {
-			name := entry.Name()
-			if strings.HasSuffix(name, ".tar.gz") {
-				log.Println(name)
-				file, err := os.Open("public/predictions/" + name)
-				Check(err)
-				defer file.Close()
-				ImportFile(file, db)
-			}
+	for _, entry := range entries {
+		name := entry.Name()
+		if strings.HasSuffix(name, ".tar.gz") {
+			log.Println(name)
+			file, err := os.Open("public/predictions/" + name)
+			Check(err) //error: truncated gzip input for human.hprdlabel.receptor.RFall.ScorLablFeaGeneInfo.allhuman.ggi.10886.addDisease.tar.gz
+			defer file.Close()
+			ImportArchivedFile(file, db)
 		}
 	}
+	//}
 	log.Println("Database is setup")
 }
 
 func ImportArchivedFile(f *os.File, db *bolt.DB) {
 	var contents []byte
-	var lines [][]byte
-	var entries []byte
 
-	reader, err = gzip.NewReader(f)
+	gzipReader, err := gzip.NewReader(f)
 	Check(err)
-	reader := tar.NewReader(reader)
+	reader := tar.NewReader(gzipReader)
 	header, err := reader.Next()
 	Check(err)
 
-	if header.Typeflag == tar.TypeReg {
-		if contents, err = ioutil.ReadAll(reader); err != nil {
-			Check(err)
-		}
-		lines := bytes.Split(contents, '\n')
-	} else {
+	if header.Typeflag != tar.TypeReg {
 		log.Printf("Unexpected file type in archive: $s!", header.Name)
 		return
 	}
 
+	if contents, err = ioutil.ReadAll(reader); err != nil {
+		Check(err)
+	}
+	lines := bytes.Split(contents, []byte("\n"))
+
 	db.Update(func(tx *bolt.Tx) error {
-		graphBucket := tx.Bucket(graphBucketName)
-		for _, line := range lines {
-			items := bytes.Split(line, '\t')
-			dump := GraphEntry{,""}
-			err = graphBucket.put(items[1], dump)
+		graphBucket := tx.Bucket([]byte(graphBucketName))
+		for _, line := range lines[1:] {
+			if len(line) == 0 {
+				continue
+			}
+			items := bytes.Split(line, []byte("\t"))
+			score, err := strconv.ParseFloat(string(items[4]), 64)
 			Check(err)
+			entry := GraphEntry{string(items[3]), score}
+			Check(err)
+			if key := items[1]; len(key) > 0 {
+				err = graphBucket.Put(key, entry.Encode())
+				Check(err)
+			}
 		}
+		return nil
 	})
 }
 
