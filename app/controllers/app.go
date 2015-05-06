fe57d7b3ed1ebabffc5043a96f2cd68a95e3ad7b
Nick Janus <nickjanus@gmail.com>
2015-05-06 14:48:17 -0400

    Adds validation

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -6,7 +6,9 @@ import (
 	"github.com/nickjanus/ProteinGraphQuery/app/models"
 	"github.com/revel/revel"
 	"log"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -19,10 +21,25 @@ func (c App) Index() revel.Result {
 }
 
 func (c App) Query(query, cutoff string) revel.Result {
-	n, err := strconv.ParseFloat(cutoff, 64)
-	if err != nil {
-		log.Fatal("Could not parse cutoff argument")
+	query = strings.Trim(query, "\t ")
+	c.Validation.Required(query).Message("Please enter a query.")
+	c.Validation.Required(cutoff).Message("Please specify a cutoff")
+	c.Validation.Match(query, regexp.MustCompile("^[[:word:]]+(,[[:space:]]*[[:word:]]+)*$")).
+		Message("Please separate multiple items with commas.")
+	c.Validation.Match(cutoff, regexp.MustCompile("^[-.0-9]+$")).
+		Message("Cutoff must be specified as a number.")
+	n, _ := strconv.ParseFloat(cutoff, 64)
+	if n < -0.5 {
+		c.Validation.Error("Cutoff must be greater than -0.5")
 	}
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	query = strings.Replace(query, " ", "", -1)
+	queries := strings.Split(query, ",")
 
 	//TODO share db connection between requests
 	db, err := bolt.Open(models.DatabaseName, 0600, &bolt.Options{Timeout: 5 * time.Second})
@@ -31,7 +48,7 @@ func (c App) Query(query, cutoff string) revel.Result {
 	}
 	defer db.Close()
 
-	graph := models.RetrieveSubgraph(db, query, n)
+	graph := models.RetrieveSubgraph(db, queries, n)
 	graphEnc, err := json.Marshal(graph)
 	if err != nil {
 		log.Fatal("Cannot encode resulting graph")
